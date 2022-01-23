3cd5001c84d217541855514c29937a94451f9359
Syphax Chergui <is_chergui@esi.dz>
2022-01-23 10:09:20 +0100

    Md

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className='mx-auto w-10/12 sm:w-9/12 lg:8/12 '>
       <header className=''>
-        <h1 className='text-6xl font-bold text-center mt-8 mb-6'>MyBlog</h1>
+        <h1 className='text-6xl font-bold text-center mt-8 mb-6'>My Blog</h1>
         <nav>
           <ul className='flex justify-center space-x-4'>
             <li>
